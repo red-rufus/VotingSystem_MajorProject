9328d0cf54c5ad8376a8820d00dd0ebefca0a122
travistanner <rajatgauri21@gmail.com>
2018-02-14 13:42:44 +0530

    Rigging check, fixed user identification.

--- source.cpp
+++ source.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 char pword[10] = {"rajat@123"};
 char choice;
-int uno, total=0;
+int uno, total=0, count1=0, count2=0, count3=0, count4=0, count5=0;
 
 void message();
 void login();
@@ -27,6 +27,7 @@ int main()
         cin>>choice;
         information_desk(choice);
         system("cls");
+        identify:
         cout<<"Enter your user no.: ";
         cin>>uno;
         system("cls");
@@ -37,6 +38,8 @@ int main()
             case 102: user3(); break;
             case 103: user4(); break;
             case 104: user5(); break;
+            default: cout<<"User not found in the database.";
+                    goto identify;
         }
     }
 }
@@ -85,96 +88,116 @@ void information_desk(char choice)
 
 void user1()
 {
-    char one[5] = {"6029"};
-    char one1[5];
-    cout<<"Hello user 1!"<<endl;
-    jump:
-    cout<<"Enter CNIC no.: ";
-    cin>>one1;
-    system("cls");
-    if(strcmp(one, one1)==0)
+    if(count1 == 0)
     {
-        cout<<"CNIC matched."<<endl;
+        char one[5] = {"6029"};
+        char one1[5];
+        cout<<"Hello user 1!"<<endl;
+        jump:
+        cout<<"Enter CNIC no.: ";
+        cin>>one1;
+        system("cls");
+        if(strcmp(one, one1)==0)
+        {
+            cout<<"CNIC matched."<<endl;
+        }
+        else
+        {
+            cout<<"CNIC not matched."<<endl;
+            goto jump;
+        }
     }
-    else
-    {
-        cout<<"CNIC not matched."<<endl;
-        goto jump;
-    }
+    else{cout<<"Sorry you have casted your vote!";}
 }
 void user2()
 {
-    char two[5] = {"6029"};
-    char two2[5];
-    cout<<"Hello user 2!"<<endl;
-    jump:
-    cout<<"Enter CNIC no.: ";
-    cin>>two2;
-    system("cls");
-    if(strcmp(two, two2)==0)
+    if(count2 == 0)
     {
-        cout<<"CNIC matched."<<endl;
+        char two[5] = {"6029"};
+        char two2[5];
+        cout<<"Hello user 2!"<<endl;
+        jump:
+        cout<<"Enter CNIC no.: ";
+        cin>>two2;
+        system("cls");
+        if(strcmp(two, two2)==0)
+        {
+            cout<<"CNIC matched."<<endl;
+        }
+        else
+        {
+            cout<<"CNIC not matched."<<endl;
+            goto jump;
+        }
     }
-    else
-    {
-        cout<<"CNIC not matched."<<endl;
-        goto jump;
-    }
+    else{cout<<"Sorry you have casted your vote!";}
 }
 void user3()
 {
-    char three[5] = {"6029"};
-    char three3[5];
-    cout<<"Hello user 3!"<<endl;
-    jump:
-    cout<<"Enter CNIC no.: ";
-    cin>>three3;
-    system("cls");
-    if(strcmp(three, three3)==0)
+    if(count3 == 0)
     {
-        cout<<"CNIC matched."<<endl;
+        char three[5] = {"6029"};
+        char three3[5];
+        cout<<"Hello user 3!"<<endl;
+        jump:
+        cout<<"Enter CNIC no.: ";
+        cin>>three3;
+        system("cls");
+        if(strcmp(three, three3)==0)
+        {
+            cout<<"CNIC matched."<<endl;
+        }
+        else
+        {
+            cout<<"CNIC not matched."<<endl;
+            goto jump;
+        }
     }
-    else
-    {
-        cout<<"CNIC not matched."<<endl;
-        goto jump;
-    }
+    else{cout<<"Sorry you have casted your vote!";}
 }
 void user4()
 {
-    char four[5] = {"6029"};
-    char four4[5];
-    cout<<"Hello user 4!"<<endl;
-    jump:
-    cout<<"Enter CNIC no.: ";
-    cin>>four4;
-    system("cls");
-    if(strcmp(four, four4)==0)
+    if(count4 == 0)
     {
-        cout<<"CNIC matched."<<endl;
+        char four[5] = {"6029"};
+        char four4[5];
+        cout<<"Hello user 4!"<<endl;
+        jump:
+        cout<<"Enter CNIC no.: ";
+        cin>>four4;
+        system("cls");
+        if(strcmp(four, four4)==0)
+        {
+            cout<<"CNIC matched."<<endl;
+        }
+        else
+        {
+            cout<<"CNIC not matched."<<endl;
+            goto jump;
+        }
     }
-    else
-    {
-        cout<<"CNIC not matched."<<endl;
-        goto jump;
-    }
+    else{cout<<"Sorry you have casted your vote!";}
 }
 void user5()
 {
-    char five[5] = {"6029"};
-    char five5[5];
-    cout<<"Hello user 5!"<<endl;
-    jump:
-    cout<<"Enter CNIC no.: ";
-    cin>>five5;
-    system("cls");
-    if(strcmp(five, five5)==0)
+    if(count5 == 0)
     {
-        cout<<"CNIC matched."<<endl;
+        char five[5] = {"6029"};
+        char five5[5];
+        cout<<"Hello user 5!"<<endl;
+        jump:
+        cout<<"Enter CNIC no.: ";
+        cin>>five5;
+        system("cls");
+        if(strcmp(five, five5)==0)
+        {
+            cout<<"CNIC matched."<<endl;
+        }
+        else
+        {
+            cout<<"CNIC not matched."<<endl;
+            goto jump;
+        }
     }
-    else
-    {
-        cout<<"CNIC not matched."<<endl;
-        goto jump;
-    }
+    else{cout<<"Sorry you have casted your vote!";}
 }
